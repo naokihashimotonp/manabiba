0f129d9b5ef336041b059c097322446886b48161
naokihashimotonp <nhashimoto@netprotections.co.jp>
2018-10-09 10:46:38 +0900

    for debug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def top
-    @posts = Post.all.order(created_at: :desc)
-    #render html: "Hello, World"
+    #@posts = Post.all.order(created_at: :desc)
+    render html: "Hello, World"
   end
 end
